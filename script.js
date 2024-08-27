bcb430b08e03a65e241222b6f2a738ef051a0949
juanma-dev <juanmanuelcastellanoshernandez@gmail.com>
2024-08-27 14:34:37 -0500

    Cuba BCI Version 1.1.1

--- script.js
+++ script.js
@@ -7,19 +7,3 @@ function closeSidebar(){
     sidebar.style.display = 'none'
 }
 
-function spanish(){
-    [].forEach.call(document.querySelectorAll('.en'), function (el) {
-            el.style.display = "none";
-        });
-    [].forEach.call(document.querySelectorAll('.es'), function (el) {
-            el.style.display = "block";
-        });
-}
-function english(){
-    [].forEach.call(document.querySelectorAll('.es'), function (el) {
-            el.style.display = "none";
-        });
-    [].forEach.call(document.querySelectorAll('.en'), function (el) {
-            el.style.display = "block";
-        });
-}
